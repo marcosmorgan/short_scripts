091d38af9e3ea9f838c929da72d445fd8186f2c1
Marcos Morgan <marcosmorgan@gmail.com>
2018-01-25 13:54:35 +0000

    Fix HOMER missing file bug

--- HOMER/scripts/configuration/leukemia.py
+++ HOMER/scripts/configuration/leukemia.py
@@ -1,6 +1,4 @@
 PATH       = "/Volumes/Promise_Pegasus/marcos_dropbox/Dropbox/Lab/Marcos_files/Scripts_Data/scripts/peaks/views/"
 EXPERIMENT = "leukemia"
 SAMPLES    = ["control", "experiment", "sample_1",  "sample_2",  "sample_3"]
-SAMPLES    = ["CKIT"]
-
-
+SAMPLES    = ["CKIT", "control"]
--- HOMER/scripts/snakemake.py
+++ HOMER/scripts/snakemake.py
@@ -8,7 +8,7 @@ path=PATH
 
 rule all:
   input:
-      expand("../results/{experiment}/peaks_motifOutput_{sample}/homerResults/motif1.motif1", experiment=EXPERIMENT, sample=SAMPLES)
+      expand("../results/{experiment}/peaks_motifOutput_{sample}/homerResults/motif1.motif", experiment=EXPERIMENT, sample=SAMPLES)
 
 rule run_shuffleBed:
   input:
@@ -25,7 +25,7 @@ rule run_find_motifs:
       expand("../views/{experiment}/shuffledBeds/mm10_refseq_shuffled_{sample}.bed", experiment=EXPERIMENT, sample=SAMPLES),
       expand("{path}{experiment}/exomePeaks_views/{sample}/peak.bed", path=PATH, experiment=EXPERIMENT, sample=SAMPLES)
   output:
-      "../results/{experiment}/peaks_motifOutput_{sample}/homerResults/motif1.motif1"
+      "../results/{experiment}/peaks_motifOutput_{sample}/homerResults/motif1.motif"
   shell:
       "findMotifsGenome.pl {path}{wildcards.experiment}/exomePeaks_views/{wildcards.sample}/peak.bed mm10 ../results/{wildcards.experiment}/peaks_motifOutput_{wildcards.sample} -size 200 -len 6 -mask -bg ../views/{wildcards.experiment}/shuffledBeds/mm10_refseq_shuffled_{wildcards.sample}.bed"
 
