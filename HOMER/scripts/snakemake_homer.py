7cd18d867c955d0b941a795018562eb6c962e20f
Marcos Morgan <marcosmorgan@gmail.com>
2019-02-12 18:48:19 +0000

    Use shaffle bed for HOMER analysis

--- HOMER/scripts/snakemake_homer.py
+++ HOMER/scripts/snakemake_homer.py
@@ -6,7 +6,7 @@ rule all:
           project=config["project"]["homer"], 
           sample=config["samples"])
 
-rule run_shuffleBed:
+rule intersect_beds:
   input:
       expand("../../../peaks/gene_body/projects/{project_peak}/views/{caller}/{sample}/{file}", 
           caller = config["peaks"]["caller"], 
@@ -14,7 +14,19 @@ rule run_shuffleBed:
           project_peak=config["project"]["peaks"], 
           sample=config["samples"])   
   output:
-      "../projects/{project}/views/shuffledBeds/shuffled_{sample}.bed"
+      "../projects/{project}/views/shuffledBeds/intersected_{sample}.bed"
+  params:
+      reference = lambda wildcards : config["samples"][wildcards.sample]["reference"]
+  shell:
+      "intersectBed  \
+      -a {input} \
+      -b {params.reference} > {output}"
+
+rule run_shuffleBed:
+  input:
+      "../projects/{project}/views/shuffledBeds/intersected_{sample}.bed"
+  output:
+      "../projects/{project}/views/shuffledBeds/shuffledIntersected_{sample}.bed"
   params:
       reference = lambda wildcards : config["samples"][wildcards.sample]["reference"],
       chormosomes = lambda wildcards : config["samples"][wildcards.sample]["chormosomes"]
@@ -25,12 +37,9 @@ rule run_shuffleBed:
 
 rule run_find_motifs:
   input:
-      peaks = expand("../../../peaks/gene_body/projects/{project_peak}/views/{caller}/{sample}/{file}", 
-          caller = config["peaks"]["caller"], 
-          file = config["peaks"]["file"], 
-          project_peak=config["project"]["peaks"], 
-          sample=config["samples"]),
-      shuffled = "../projects/{project}/views/shuffledBeds/shuffled_{sample}.bed"
+      peaks = "../projects/{project}/views/shuffledBeds/intersected_{sample}.bed",
+      shuffled = "../projects/{project}/views/shuffledBeds/shuffledIntersected_{sample}.bed"
+      #shuffled = "../projects/{project}/views/shuffledBeds/shuffledIntersected_sample4.bed"
   output:
       "../projects/{project}/results/{sample}/homerMotifs.all.motifs"
   params:
@@ -38,9 +47,23 @@ rule run_find_motifs:
       genome = lambda wildcards : config["samples"][wildcards.sample]["genome"]
   shell:
       "findMotifsGenome.pl {input.peaks} {params.genome} {params.out} \
-      -size 200 \
-      -len 6 \
-      -mask \
+      -size 450 \
+      -len 5 \
+      -S 3 \
+      -p 4 \
       -bg {input.shuffled} \
+      -mask \
+      -cpg \
       -rna"
 
+# sample123
+# sample456
+#       "findMotifsGenome.pl {input.peaks} {params.genome} {params.out} \
+#       -size 400 \
+#       -len 6 \
+#       -S 3 \
+#       -p 4 \
+#       -bg {input.shuffled} \
+#       -mask \
+#       -cpg \
+#       -rna"
